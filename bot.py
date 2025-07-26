1faf04d42b39777a64224e161707893936609680
Daniel Velasco <divelasco7@gmail.com>
2025-07-26 00:51:30 -0500

    fix: error

--- bot.py
+++ bot.py
@@ -18,30 +18,25 @@ headers = {
     'Authorization': f'Bearer {os.getenv("DOTLIST_JWT")}',
 }
 
-params = {
-
-}
-
-response = requests.get(
-    'https://polished-beagle-841.convex.site/health',
-    params=params,
-    headers=headers
-)
-
-#rdict = json.loads(response.text)
-#print(json.dumps(rdict, indent=4))
-
-print(response)
+userCount = json.loads(requests.get('https://polished-beagle-841.convex.site/users').text).get("totalUsers")
 
 @client.event
 async def on_ready():
-    await client.change_presence(status=discord.Status.online, activity=discord.Activity(name=f"custom", type=discord.ActivityType.custom, state="Doing fun things"))
+    await client.change_presence(status=discord.Status.online, activity=discord.Activity(name=f"custom", type=discord.ActivityType.custom, state=f"dotbotting with {userCount} users"))
     await tree.sync()
     print(f'Logged in as {client.user}')
     
 @tree.command(name="test", description="A test command")
 async def test_command(interaction: discord.Interaction):
-    await interaction.response.send_message("dotbotting")
+    params = {
+
+    }
+    response = requests.get(
+        'https://polished-beagle-841.convex.site/lists/k97bff2sazfcygnhyddgtx5r857mc207/items',
+        params=params,
+        headers=headers
+    )
+    await interaction.response.send_message(json.dumps(json.loads(response.text), indent=4))
     print(f'Command run by {interaction.user.name}')
 
 client.run(os.getenv("BOT_TOKEN"))
